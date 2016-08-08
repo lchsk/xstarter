9dc691e48d4aee75d87b1c03b434f2a46cfb801c
lchsk <mjlechowski@gmail.com>
2016-08-08 22:22:39 +0100

    Add tests
    Add criterion dependency to an addition executable.
    
    Tests should be in tests/ directory.
    
    A single tests file added (expand_tilde.c)

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Cleaning xstarter directory..."
 rm -f Makefile
 rm -f CMakeCache.txt
 rm -f cmake_install.cmake
@@ -8,3 +9,12 @@ rm -f install_manifest.txt
 rm -f nohup.out
 rm -rf CMakeFiles
 rm -rf ./bin
+
+echo "Cleaning tests..."
+
+cd tests; \
+rm -f Makefile; \
+rm -f CMakeCache.txt; \
+rm -rf CMakeFiles; \
+rm -rf cmake_install.cmake; \
+rm -rf xstarter_tests
